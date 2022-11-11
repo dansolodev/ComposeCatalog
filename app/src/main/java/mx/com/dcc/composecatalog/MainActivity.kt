7fea1afcef3284eab35bab0443cf77e7f467cdec
Daniel Cinto <dansolo.cc@gmail.com>
2022-11-11 11:39:48 -0600

    spcer, state, recompiscion, remember, state, texts

--- app/src/main/java/mx/com/dcc/composecatalog/MainActivity.kt
+++ app/src/main/java/mx/com/dcc/composecatalog/MainActivity.kt
@@ -8,15 +8,25 @@ import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import mx.com.dcc.composecatalog.ui.theme.ComposeCatalogTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,10 +39,57 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MyComplexLayout()
+                    MyText()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MyText() {
+    Column(Modifier.fillMaxSize()) {
+        Text(text = "Esto es un ejemplo")
+        Text(text = "Esto es un ejemplo", color = Color.Blue)
+        Text(text = "Esto es un ejemplo", fontWeight = FontWeight.ExtraBold)
+        Text(text = "Esto es un ejemplo", fontWeight = FontWeight.Light)
+        Text(text = "Esto es un ejemplo", style = TextStyle(fontFamily = FontFamily.Cursive))
+        Text(
+            text = "Esto es un ejemplo",
+            style = TextStyle(textDecoration = TextDecoration.LineThrough)
+        )
+        Text(
+            text = "Esto es un ejemplo",
+            style = TextStyle(textDecoration = TextDecoration.Underline)
+        )
+        Text(
+            text = "Esto es un ejemplo", style = TextStyle(
+                textDecoration = TextDecoration.combine(
+                    listOf(TextDecoration.Underline, TextDecoration.LineThrough)
+                )
+            )
+        )
+        Text(text = "Esto es un ejemplo", fontSize = 30.sp)
+    }
+}
+
+@Composable
+fun MyStateExample() {
+    // remember matiene el estado con el composable se recrea, pero no cuando la activity se destruye y se vuelve a crear
+    // rememberSaveable si mantiene el estado aun que la activity se recree.
+    // by se covina de var para que no tengamos que acceder al .value
+    var counter by rememberSaveable {
+        mutableStateOf(0)
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { counter += 1 }) {
+            Text(text = "Pulsar")
+        }
+        Text(text = "He sido pulsado ${counter} veces")
     }
 }
 
@@ -43,10 +100,12 @@ fun MyComplexLayout() {
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(1f)
-                .background(Color.Cyan)
+                .background(Color.Cyan),
+            contentAlignment = Alignment.Center
         ) {
-
+            Text(text = "Ejemplo 1")
         }
+        Spacer(modifier = Modifier.height(30.dp))
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -56,8 +115,11 @@ fun MyComplexLayout() {
                 modifier = Modifier
                     .weight(1f)
                     .fillMaxHeight()
-                    .background(Color.Red)
-            )
+                    .background(Color.Red),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Ejemplo 2")
+            }
             Box(
                 modifier = Modifier
                     .weight(1f)
@@ -65,23 +127,19 @@ fun MyComplexLayout() {
                     .background(Color.Green),
                 contentAlignment = Alignment.Center
             ) {
-                Text(text = "Hello world")
+                Text(text = "Ejemplo 3")
             }
         }
         Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(1f)
-                .background(Color.Magenta)
+                .background(Color.Magenta),
+            contentAlignment = Alignment.BottomCenter
         ) {
-
+            Text(text = "Ejemplo 4")
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Composable
@@ -159,6 +217,6 @@ fun MyRow() {
 @Composable
 fun DefaultPreview() {
     ComposeCatalogTheme {
-        MyComplexLayout()
+        MyText()
     }
 }
